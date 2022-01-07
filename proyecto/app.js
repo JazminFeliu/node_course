aed93013fe73351780b17e7dd5d30526bcd21e6e
JazminFeliu <jazminfeliu@gmail.com>
2022-01-07 14:24:53 -0300

    subo un ejemplo con body-parser

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,7 +1,10 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.json());  //para peticiones application/json
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "jade");
 
 app.get("/", function(req, res){
@@ -11,6 +14,12 @@ app.get("/", function(req, res){
 app.get("/login", function(req, res){
     res.render("login");
 });
+
+app.post("/users", function(req, res){
+    console.log("Contraseña: "+ req.body.password);
+    console.log("Email: "+ req.body.email);
+    res.send("Recibimos tus datos");
+})
 
 
 app.listen(8080);
