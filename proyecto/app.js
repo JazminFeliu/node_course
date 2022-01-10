d33e23f9932468808cb3a3e89e4602904baa8e5c
JazminFeliu <jazminfeliu@gmail.com>
2022-01-10 09:53:24 -0300

    subo configuracion de Mongodb

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+    email: String,
+    password: String
+
+};
+
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());  //para peticiones application/json
@@ -12,14 +26,20 @@ app.get("/", function(req, res){
 });
 
 app.get("/login", function(req, res){
-    res.render("login");
+    User.find(function(err,doc){
+        console.log(doc);
+        res.render("login");
+    });    
 });
 
 app.post("/users", function(req, res){
-    console.log("Contraseña: "+ req.body.password);
-    console.log("Email: "+ req.body.email);
-    res.send("Recibimos tus datos");
-})
+    var user = new User({email: req.body.email, password: req.body.password});
+
+    user.save(function(){
+        res.send("Guardamos tus datos");
+    });  
+    
+});
 
 
 app.listen(8080);
