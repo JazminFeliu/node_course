46aa022520fb175fb192bd80c9f8a337d2905464
JazminFeliu <jazminfeliu@gmail.com>
2022-01-12 12:05:57 -0300

    subo configuracion de session

--- proyecto/app.js
+++ proyecto/app.js
@@ -2,14 +2,23 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var User = require("./models/user").User;
+var session = require("express-session");
 
 
 app.use(express.static('public'));
 app.use(bodyParser.json());  //para peticiones application/json
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: "98754asdf789rerd",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set("view engine", "jade");
 
+
 app.get("/", function(req, res){
+    console.log(req.session.user_id);
     res.render("index");
 });
 
@@ -26,10 +35,11 @@ app.get("/login", function(req, res){
 
 app.post("/sessions", function(req, res){
 
-    User.findOne({email:req.body.email,password:req.body.password},function(err,docs){
-        console.log(docs);
-        res.send("Hola Jupiter");
-    })
+    User.findOne({email:req.body.email,password:req.body.password},function(err,user){
+        req.session.user_id = user._id;
+        res.send("Hola Jupiter again!");
+
+    });
     
 });
 
